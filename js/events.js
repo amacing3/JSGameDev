9d7874f2a988208509ad721842200a2cf392398b
Emma Cluff <dragonbotguardian@gmail.com>
2016-09-28 11:56:10 -0600

    event stuff

---
+++ js/events.js
@@ -0,0 +1,15 @@
+var button = document.getElementById("button1");
+
+var myFunction = function () {
+    alert("Hello World");
+}
+
+button.addEventListener ("click", myFunction);
+
+var startButton = document.getElementById("startGame");
+
+var startGameHandler = function startMyGame() {
+    prompt("Enter your name.");
+}
+
+startButton.addEventListener("click", startGameHandler);
